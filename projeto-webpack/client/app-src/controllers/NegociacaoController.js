7ee0198211d24cac36d822944dd6c963ce3d2abf
pedro.souza <pedro.souza@fasolti.com.br>
2018-01-18 08:32:26 -0200

    Ajustes no projeto

--- projeto-webpack/client/app-src/controllers/NegociacaoController.js
+++ projeto-webpack/client/app-src/controllers/NegociacaoController.js
@@ -1,4 +1,4 @@
-import { Negociacoes, NegociacaoService, Negociacao } from '../domain/index.js';
+import { Negociacoes, Negociacao } from '../domain/index.js';
 import { NegociacoesView, MensagemView, Mensagem, DateConverter } from '../ui/index.js';
 import { getNegociacaoDao, Bind, getExceptionMessage, debounce, controller, bindEvent } from '../util/index.js';
 
@@ -20,8 +20,6 @@ export class NegociacaoController {
             new MensagemView('#mensagemView'),
             'texto'
         );
-
-        this._service = new NegociacaoService();
 
         this._init();
     }
@@ -74,8 +72,10 @@ export class NegociacaoController {
     @debounce()
     async importaNegociacoes() {
 
+        const { NegociacaoService } = await System.import('../domain/negociacao/NegociacaoService');
+
         try {
-            const negociacoes = await this._service.obtemNegociacoesDoPeriodo();
+            const negociacoes = await service.obtemNegociacoesDoPeriodo();
             console.log(negociacoes);
             negociacoes.filter(novaNegociacao =>
 
